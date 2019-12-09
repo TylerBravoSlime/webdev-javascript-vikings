434011eb4e7c9030aa28ccb31a6e8031b2dc21f3
Tyler Bravo <22049@columbushs.com>
2019-12-09 08:46:32 -0500

    need to

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,16 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health  = health
+        this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.damage-= health
+
+    }
 
 // Viking
 class Viking {}
